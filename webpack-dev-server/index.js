0ba22ca7312a23c7a0917e168a40d64fd56b39c3
Leonidas Esteban <leonidasesteban@gmail.com>
2019-05-22 00:55:18 -0500

    webpack-dev-server updated

--- webpack-dev-server/index.js
+++ webpack-dev-server/index.js
@@ -1,5 +1,5 @@
 import './estilos.css'
 
-document.write('sfsdfds hola mundo!')
+document.write('hola mundo!')
 // console.log('Hola mundo!, desde Webpack');
 console.log('Hola mundo!, desde Webpack en un webpack.config');
--- webpack-dev-server/webpack.config.js
+++ webpack-dev-server/webpack.config.js
@@ -1,12 +1,14 @@
 const path = require('path');
 
 module.exports = {
+  mode: 'development',
   entry: path.resolve(__dirname, 'index.js'),
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
   },
   devServer: {
+    open: true,
     port: 9000,
   },
   module: {
