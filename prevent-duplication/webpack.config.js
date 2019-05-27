aaa6708ffb711bf738768d957e4d4fabc4d8c47a
Leonidas Esteban <leonidasesteban@gmail.com>
2019-05-26 21:33:53 -0500

    update splintChunks

--- prevent-duplication/webpack.config.js
+++ prevent-duplication/webpack.config.js
@@ -1,16 +1,17 @@
 const path = require('path');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const webpack = require('webpack');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: {
     home: path.resolve(__dirname, 'src/js/index.js'),
-    contact: path.resolve(__dirname, 'src/js/contact.js')
+    contact: path.resolve(__dirname, 'src/js/contact.js'),
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: '[name].js'
+    filename: '[name].js',
+    publicPath: 'dist/'
   },
+  devtool: 'inline-source-map',
   module: {
     rules: [
       // Aquí van los loaders
@@ -18,103 +19,73 @@ module.exports = {
         // test: que tipo de archivo quiero reconocer,
         // use: que loader se va a encargar del archivo
         test: /\.js$/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['es2015', 'react']
-          }
-        },
+        use: 'babel-loader',
+        exclude: /node_modules/,
       },
       {
-        test: /\.(jpg|png|gif|woff|eot|ttf|svg)$/,
+        test: /\.(jpg|png|gif|woff|eot|ttf|svg|mp4|webm)$/,
         use: {
           loader: 'url-loader',
           options: {
-            limit: 1000000,
+            limit: 90000,
           }
         }
       },
       {
-        // test: que tipo de archivo quiero reconocer,
-        // use: que loader se va a encargar del archivo
         test: /\.css$/,
-        use: ExtractTextPlugin.extract({
-          // ['style-loader','css-loader']
-          // fallback: 'style-loader',
-          use: [
-            {
-              loader: 'css-loader',
-              options: {
-                modules: true,
-                importLoaders: 1
-              }
-            },
-            'postcss-loader'
-          ]
-        }),
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader
+          },
+          // 'css-loader',
+          { loader: 'css-loader', options: { importLoaders: 1 } },
+          'postcss-loader',
+        ]
       },
       {
-        // test: que tipo de archivo quiero reconocer,
-        // use: que loader se va a encargar del archivo
         test: /\.scss$/,
-        use: ExtractTextPlugin.extract({
-          // ['style-loader','css-loader']
-          // fallback: 'style-loader',
-          use: ["css-loader", "sass-loader"]
-        }),
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader
+          },
+          "css-loader",
+          "sass-loader"
+        ]
       },
       {
-        // test: que tipo de archivo quiero reconocer,
-        // use: que loader se va a encargar del archivo
         test: /\.less$/,
-        use: ExtractTextPlugin.extract({
-          // ['style-loader','css-loader']
-          // fallback: 'style-loader',
-          use: ["css-loader", {
-            loader: 'less-loader',
-            options: {
-              noIeCompat: true,
-            }
-          }]
-        }),
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader
+          },
+          "css-loader",
+          "less-loader"
+        ]
       },
       {
-        // test: que tipo de archivo quiero reconocer,
-        // use: que loader se va a encargar del archivo
         test: /\.styl$/,
-        use: ExtractTextPlugin.extract({
-          // ['style-loader','css-loader']
-          // fallback: 'style-loader',
-          use: [
-            "css-loader",
-            {
-              loader: 'stylus-loader',
-              options: {
-                use: [
-                  require('nib'),
-                  require('rupture')
-                ],
-                import: [
-                  '~nib/lib/nib/index.styl',
-                  '~rupture/rupture/index.styl'
-                ]
-              }
-            }
-          ]
-        }),
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader
+          },
+          "css-loader",
+          "stylus-loader"
+        ]
       }
     ]
   },
   plugins: [
     // aquí van los plugins
-    new ExtractTextPlugin("css/[name].css"),
-    // new webpack.optimize.CommonsChunkPlugin({
-    //   name: 'common'
-    // })
+    new MiniCssExtractPlugin({
+      filename: "css/[name].css",
+      chunkFilename: 'css/[id].css',
+    }),
   ],
   optimization: {
     splitChunks: {
-        name: 'common',
-    },
+      chunks: 'all',
+      minSize: 0,
+      name: 'commons'
+    }
   }
 }
