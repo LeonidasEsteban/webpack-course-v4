725a29865b3e7e2a6a1a5ab8d4deceeb3668daaf
Leonidas Esteban <leonidasesteban@gmail.com>
2019-05-26 19:05:32 -0500

    [update] preprocesor

---
+++ prepro/src/js/index.js
@@ -0,0 +1,10 @@
+import '../css/estilos.css'
+
+import React from 'react';
+import { render } from 'react-dom';
+import App from './components/app'
+
+render(<App />, document.getElementById('container'));
+
+
+
---
+++ prepro/webpack.config.js
@@ -0,0 +1,81 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: path.resolve(__dirname, 'src/js/index.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: 'dist/'
+  },
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      // Aquí van los loaders
+      {
+        // test: que tipo de archivo quiero reconocer,
+        // use: que loader se va a encargar del archivo
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(jpg|png|gif|woff|eot|ttf|svg|mp4|webm)$/,
+        use: {
+          loader: 'url-loader',
+          options: {
+            limit: 90000,
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader
+          },
+          // 'css-loader',
+          { loader: 'css-loader', options: { importLoaders: 1 } },
+          'postcss-loader',
+        ]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader
+          },
+          "css-loader",
+          "sass-loader"
+        ]
+      },
+      {
+        test: /\.less$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader
+          },
+          "css-loader",
+          "less-loader"
+        ]
+      },
+      {
+        test: /\.styl$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader
+          },
+          "css-loader",
+          "stylus-loader"
+        ]
+      }
+    ]
+  },
+  plugins: [
+    // aquí van los plugins
+    new MiniCssExtractPlugin({
+      filename: "css/[name].css",
+      chunkFilename: '[id].css',
+    }),
+  ]
+}
